282ca10ae69e87990f4485c60870de62df7a5d84
kiranmanektech <70325370+kiranmanektech@users.noreply.github.com>
2022-04-15 20:17:30 +0530

    Design Changes

--- assets/js/developer.js
+++ assets/js/developer.js
@@ -1,5 +1,12 @@
+/**
+ * AOS Animation Initialization
+ */
 AOS.init();
 
+
+/**
+ * Swiper Sliders JS
+ */
 var swiper = new Swiper(".presentationSwiper", {
     slidesPerView: 1,
     spaceBetween: 0,
@@ -196,6 +203,10 @@ var swiper = new Swiper(".mockupSwiper", {
     },
 });
 
+
+/**
+ * File Upload JS
+ */
 var input = document.getElementById( 'file-upload' );
 var infoArea = document.getElementById( 'file-upload-filename' );
 
@@ -212,3 +223,74 @@ function showFileName( event ) {
   // use fileName however fits your app best, i.e. add it into a div
   infoArea.textContent = 'File name: ' + fileName;
 }
+
+
+/**
+ * Show/Hide Password JS
+ */
+const togglePassword = document.querySelector("#togglePassword");
+const togglePassword1 = document.querySelector("#togglePassword1");
+const togglePassword2 = document.querySelector("#togglePassword2");
+const password = document.querySelector("#passwordField");
+const password1 = document.querySelector("#passwordField1");
+const password2 = document.querySelector("#passwordField2");
+
+togglePassword.addEventListener("click", function () {
+    // toggle the type attribute
+    const type = password.getAttribute("type") === "password" ? "text" : "password";
+    password.setAttribute("type", type);
+    
+    // toggle the icon
+    this.classList.toggle("icon-visibility-on");
+});
+togglePassword1.addEventListener("click", function () {
+    // toggle the type attribute
+    const type = password.getAttribute("type") === "password" ? "text" : "password";
+    password1.setAttribute("type", type);
+    
+    // toggle the icon
+    this.classList.toggle("icon-visibility-on");
+});
+togglePassword2.addEventListener("click", function () {
+    // toggle the type attribute
+    const type = password.getAttribute("type") === "password" ? "text" : "password";
+    password2.setAttribute("type", type);
+    
+    // toggle the icon
+    this.classList.toggle("icon-visibility-on");
+});
+
+// prevent form submit
+const form = document.querySelector("form");
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+});
+
+
+/**
+ * OTP Field
+ */
+ $('.digit-group').find('input').each(function() {
+	$(this).attr('maxlength', 1);
+	$(this).on('keyup', function(e) {
+		var parent = $($(this).parent());
+		
+		if(e.keyCode === 8 || e.keyCode === 37) {
+			var prev = parent.find('input#' + $(this).data('previous'));
+			
+			if(prev.length) {
+				$(prev).select();
+			}
+		} else if((e.keyCode >= 48 && e.keyCode <= 57) || (e.keyCode >= 65 && e.keyCode <= 90) || (e.keyCode >= 96 && e.keyCode <= 105) || e.keyCode === 39) {
+			var next = parent.find('input#' + $(this).data('next'));
+			
+			if(next.length) {
+				$(next).select();
+			} else {
+				if(parent.data('autosubmit')) {
+					parent.submit();
+				}
+			}
+		}
+	});
+});
